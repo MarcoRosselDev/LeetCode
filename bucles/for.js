0456a59a739c68681f92cf9bb3a1fad187f0b6d5
Marco Rossel <andresmarcorossel@gmail.com>
2023-12-13 19:13:56 -0300

    bucles

---
+++ bucles/for.js
@@ -0,0 +1,9 @@
+//for ([expresion inicial] ; [codicional] ; [actualizacion de expresion]) {
+
+  // mientras la condicion sea true se ejecuta la logica de esta seccion de codigo
+  
+  //}
+for (let i = 0; i < array.length; i++) {
+  //const element = array[i];
+
+}
