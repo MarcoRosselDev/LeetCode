9509ee547d968b2975349c188032fd1f3f579c6b
Marco Rossel <andresmarcorossel@gmail.com>
2023-12-20 18:33:10 -0300

    001 - 7

--- 001-easy.js
+++ 001-easy.js
@@ -31,10 +31,21 @@ Output: [0,1] */
  * @return {number[]}
  */
 
-const twoSum = ( arr, target ) =>{
-  const rest = target - arr[0];
-  const a = arr.indexOf(rest, 1);
-  return [0, a]
+const twoSum = ( array, target ) =>{
+  let obj = []
+  for (let i = 0; i < array.length; i++) {
+    const rest = target - array[i];
+    const desde = i + 1;
+    console.log('rest', rest);
+    console.log('desde', desde);
+
+    const pregunta = array.indexOf(rest, desde);
+    if (pregunta > 0) {
+      obj.push(i, pregunta);
+      break;
+    }
+  }
+  return obj;
 }
 console.log(twoSum([2, 7, 11, 15], 9)); // [0,1]
 console.log(twoSum([3,2,4], 6));// [2, 2] // [1, 2]
