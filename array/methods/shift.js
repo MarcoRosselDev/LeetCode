8302bea2a53954e8c7288ee7e7b352e34f44caa2
Marco Rossel <andresmarcorossel@gmail.com>
2023-12-21 23:13:32 -0300

    shift, slice, reverse

---
+++ array/methods/shift.js
@@ -0,0 +1,14 @@
+/* elimina el primer elemento de un array y lo retorna */
+
+var miPescado = ["ángel", "payaso", "mandarín", "cirujano"];
+
+console.log("miPescado antes: " + miPescado);
+// "miPescado antes: ángel,payaso,mandarín,cirujano"
+
+var eliminado = miPescado.shift();
+
+console.log("miPescado después: " + miPescado);
+// "miPescado after: payaso,mandarín,cirujano"
+
+console.log("Elemento eliminado: " + eliminado);
+// "Elemento eliminado: ángel"
---
+++ array/methods/slice.js
@@ -0,0 +1,27 @@
+/* devuelve una copia de una parte del array,
+el array original no se modifica. */
+
+/* sintax: 
+slice()
+slice(start)
+slice(start, end) */
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+console.log(animals.slice(2));
+// Expected output: Array ["camel", "duck", "elephant"]
+
+console.log(animals.slice(2, 4));
+// Expected output: Array ["camel", "duck"]
+
+console.log(animals.slice(1, 5));
+// Expected output: Array ["bison", "camel", "duck", "elephant"]
+
+console.log(animals.slice(-2));
+// Expected output: Array ["duck", "elephant"]
+
+console.log(animals.slice(2, -1));
+// Expected output: Array ["camel", "duck"]
+
+console.log(animals.slice());
+// Expected output: Array ["ant", "bison", "camel", "duck", "elephant"]
