6dfabcb101b838ce20d80f251a7ecd02624b1f6a
Marco Rossel <andresmarcorossel@gmail.com>
2023-12-22 17:01:33 -0300

    toString, toSpliced

---
+++ array/methods/toString.js
@@ -0,0 +1,11 @@
+/* retorna un string representando el 
+array especificado y sus elementos 
+
+sintax:
+
+array.toString();
+*/
+const array1 = [1, 2, 'a', '1a'];
+
+console.log(array1.toString());
+// Expected output: "1,2,a,1a"
