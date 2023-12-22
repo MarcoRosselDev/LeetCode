497c0f6e562f43f77963cc711e18f0de9fc7311b
Marco Rossel <andresmarcorossel@gmail.com>
2023-12-22 16:29:56 -0300

    toSort, toReversed

---
+++ array/methods/toReversed.js
@@ -0,0 +1,13 @@
+/* es la contraparte del metodo reverse(),
+retorna un nuevo array con los elementos en reversa.
+
+sintsx:
+toReversed()
+*/
+
+const items = [1, 2, 3];
+console.log(items); // [1, 2, 3]
+
+const reversedItems = items.toReversed();
+console.log(reversedItems); // [3, 2, 1]
+console.log(items); // [1, 2, 3]
---
+++ array/methods/toSorted.js
@@ -0,0 +1,17 @@
+/* retorna un nuevo array con los 
+elementos ordenados en orden ascendente 
+
+sintax:
+toSorted()
+toSorted(compareFn)
+*/
+
+const months = ["Mar", "Jan", "Feb", "Dec"];
+const sortedMonths = months.toSorted();
+console.log(sortedMonths); // ['Dec', 'Feb', 'Jan', 'Mar']
+console.log(months); // ['Mar', 'Jan', 'Feb', 'Dec']
+
+const values = [1, 10, 21, 2];
+const sortedValues = values.toSorted((a, b) => a - b);
+console.log(sortedValues); // [1, 2, 10, 21]
+console.log(values); // [1, 10, 21, 2]
