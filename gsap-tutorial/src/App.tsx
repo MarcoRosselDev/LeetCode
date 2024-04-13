809061f38b0e0c7c91db3131a9ac98563a08cba1
Marco Rossel <andresmarcorossel@gmail.com>
2024-04-13 15:43:41 -0400

    gsap tutorial | 02

--- gsap-tutorial/src/App.tsx
+++ gsap-tutorial/src/App.tsx
@@ -1,13 +1,38 @@
-import { useState, useContext } from 'react'
+import { useEffect, useRef } from 'react'
 import './App.css'
+import { gsap,  } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 function App() {
-  const [count, setCount] = useState(0)
-  const ThemeContext =  useContext(null) //????
+  //const [count, setCount] = useState(0)
+  //const ThemeContext =  useContext(null) //????
+
+  type T = any
+
+  const box = useRef<T>();
+
+  useEffect(() =>{
+    gsap.to('box', {
+      duration: 3,
+      rotation: 360,
+      scale: 2,
+      scrollTrigger: {
+        trigger: '.box',
+        markers: true,
+        scrub: true
+      }
+    })
+  }, [])  
 
   return (
     <>
       <p>Hola mundo</p>
+      <div className='box-div' ref={box}>
+        <p>Hola!</p>
+      </div>
     </>
   )
 }
