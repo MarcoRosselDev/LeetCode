d5c202c1ece7095627cd394a31f9c9827001ee11
Marco Rossel <andresmarcorossel@gmail.com>
2024-04-19 02:00:38 -0400

    ts three react | 00

---
+++ 01-ts-react-three/src/components/header/Header.tsx
@@ -0,0 +1,17 @@
+import './header.css'
+
+export function Header() {
+  return (
+    <header>
+      <nav>
+        <p>Logo</p>
+        <ul>
+          <li>3D</li>
+          <li>another</li>
+          <li>cv</li>
+        </ul>
+        <p>purple mode</p>
+      </nav>
+    </header>
+  )
+}
