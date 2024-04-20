6bcf3c64f1b6ceff93dc4eeaad9ce2583ea7ee94
Marco Rossel <andresmarcorossel@gmail.com>
2024-04-20 19:52:19 -0400

    ts three react | 04

--- 01-ts-react-three/src/components/body.tsx
+++ 01-ts-react-three/src/components/body.tsx
@@ -1,19 +1,34 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { context } from "../contexto/Context";
 import { Header } from "./header/Header";
 import { Main } from "./main/Main";
 import './body.css'
+//gsap utils
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 
 export function Body() {
 
-  const {dark} = useContext(context)
-  console.log(dark);
+  const container = useRef<any>();
+
+  useGSAP(() => {
+    // gsap code here...
+    gsap.to(".box", {x: 360, duration: 3}); // <-- automatically reverted
   
+  }, { scope: container }); // <-- scope is for selector text (optional)
+
+  
+  const {dark} = useContext(context)
 // aplicar la style class si dark es type Tema
   return (
-    <div className={`body-${dark} body`}>
+    <div className={`body-${dark} body`} ref={container}>
       <Header/>
       <Main />
+      {/* <div className="box gradient-blue">
+        <Main />
+      </div> */}
       
     </div>
   )
--- 01-ts-react-three/src/components/main/Main.tsx
+++ 01-ts-react-three/src/components/main/Main.tsx
@@ -1,9 +1,11 @@
 import './main.css'
 
 export function Main() {
+  // si aplico la class .box se puede mover no nesecariamente tengo que invocar gsap en este componente!!
   return (
     <main>
-      <p>Main section</p>
+      <p className='box'>Main section</p>
+      <p>another section</p>
     </main>
   )
 }
