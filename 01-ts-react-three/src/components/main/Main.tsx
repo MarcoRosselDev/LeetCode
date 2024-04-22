abfc12e7bbde81dc7b971fb1a9125a6fde8e974c
Marco Rossel <andresmarcorossel@gmail.com>
2024-04-22 13:04:20 -0400

    ts three react | 05

--- 01-ts-react-three/src/components/main/Main.tsx
+++ 01-ts-react-three/src/components/main/Main.tsx
@@ -1,10 +1,19 @@
 import './main.css'
+import { Canvas } from '@react-three/fiber'
+import {Box} from './Box'
 
 export function Main() {
   // si aplico la class .box se puede mover no nesecariamente tengo que invocar gsap en este componente!!
+  
+  // Return view, these are regular three.js elements expressed in JSX
   return (
     <main>
-      <p className='box'>Main section</p>
+      <div className='box'>
+        <Canvas>
+          <Box position={[1.2, 0, 0]} />
+          <Box position={[-1.2, 0, 0]} />
+        </Canvas>
+      </div>
       <p>another section</p>
     </main>
   )
