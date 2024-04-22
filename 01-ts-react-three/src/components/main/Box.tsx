b90147600cdce24e44949bf095fdec5de86bc14f
Marco Rossel <andresmarcorossel@gmail.com>
2024-04-22 19:14:06 -0400

    ts three react | 08

--- 01-ts-react-three/src/components/main/Box.tsx
+++ 01-ts-react-three/src/components/main/Box.tsx
@@ -2,16 +2,23 @@
 import * as THREE from 'three'
 import { useRef, useState } from 'react'
 import { useFrame, ThreeElements } from '@react-three/fiber'
+import { Model } from '../../../Untitled'
 
 export function Box(props: ThreeElements['mesh']) {
+    const meshRef = useRef<THREE.Mesh>(null!)
+    const [hovered, setHover] = useState(false)
+    const [active, setActive] = useState(false)
+    useFrame((state, delta) => (meshRef.current.rotation.x += delta))
   // This reference will give us direct access to the mesh
-  const meshRef = useRef<THREE.Mesh>(null!)
+
+
+  /* const meshRef = useRef<THREE.Mesh>(null!)
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => (meshRef.current.rotation.x += delta))
-  // Return view, these are regular three.js elements expressed in JSX
+  // Return view, these are regular three.js elements expressed in JSX */
   return (
     <mesh
       {...props}
@@ -22,16 +29,7 @@ export function Box(props: ThreeElements['mesh']) {
       onPointerOut={(event) => setHover(false)}>
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <Model />
     </mesh>
   )
 }
-
-/* createRoot(document.getElementById('root')).render(
-  <Canvas>
-    <ambientLight intensity={Math.PI / 2} />
-    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-    <Box position={[-1.2, 0, 0]} />
-    <Box position={[1.2, 0, 0]} />
-  </Canvas>,
-) */
--- 01-ts-react-three/src/components/main/Main.tsx
+++ 01-ts-react-three/src/components/main/Main.tsx
@@ -1,6 +1,7 @@
 import './main.css'
 import { Canvas } from '@react-three/fiber'
-import {Box} from './Box'
+import { Box } from './Box'
+//import {Model} from '../../../Untitled'
 
 export function Main() {
   // si aplico la class .box se puede mover no nesecariamente tengo que invocar gsap en este componente!!
@@ -14,7 +15,8 @@ export function Main() {
           <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
           <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
           <Box position={[1.2, 0, 0]} />
-          <Box position={[-1.2, 0, 0]} />
+          {/* <Box position={[-1.2, 0, 0]} /> */}
+          {/* <Model /> */}
         </Canvas>
       </div>
       <p>another section</p>
