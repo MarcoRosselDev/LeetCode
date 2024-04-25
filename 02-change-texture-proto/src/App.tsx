5fb0f780661e03e75a76bd7bc3a445a6a974e2c0
Marco Rossel <andresmarcorossel@gmail.com>
2024-04-25 14:11:24 -0400

    ts three react 2 | 02

--- 02-change-texture-proto/src/App.tsx
+++ 02-change-texture-proto/src/App.tsx
@@ -3,20 +3,31 @@ import './App.css'
 import { Body } from './components/Body.js'
 import { Canvas } from '@react-three/fiber'
 import { HandleContext } from './contexto/handleContext.js'
+import { useContext } from 'react'
+import { Contexto } from './contexto/contextoGlobal.js'
 
 function App() {
+  const {something} = useContext(Contexto)
 
+  console.log(something);
+  
   return (
     <>
-      <HandleContext />
-
-      <Canvas>
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        <Body position={[1.2, 0, 0]} />
-        {/* <Model /> */}
-      </Canvas>
+      <HandleContext >
+        <div>
+          <button>rojo</button>
+          <button>azul</button>
+          <button>verde</button>
+          <button>amarillo</button>
+        </div>
+        <Canvas>
+          <ambientLight intensity={Math.PI / 2} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+          <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+          <Body position={[1.2, 0, 0]} />
+          {/* <Model /> */}
+        </Canvas>
+      </HandleContext >
     </>
   )
 }
--- 02-change-texture-proto/src/contexto/handleContext.tsx
+++ 02-change-texture-proto/src/contexto/handleContext.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import {Contexto} from './contextoGlobal'
 
 export function HandleContext({children}: {children: React.ReactNode}) {
 
-  console.log(Contexto);
+  const [something, setSomething] = useState(true)
+
+  function handleSomething():void{
+    setSomething(prev =>!prev)
+  }
   return (
-    <Contexto.Provider value={{'hola', [12,1]}}>
+    <Contexto.Provider value={{'hola':[12,1], something, handleSomething }}>
       {children}
     </Contexto.Provider>
   )
