9a13019af07e3b16cf23e4d1d07009ead439ad5a
Marco Rossel <andresmarcorossel@gmail.com>
2024-04-29 15:03:22 -0400

    ts three react 3 | 04 clone cube

--- 03-3d-model-1/src/componentes/Main-canvas.jsx
+++ 03-3d-model-1/src/componentes/Main-canvas.jsx
@@ -1,9 +1,12 @@
 import {Canvas} from '@react-three/fiber'
+import { CameraControls } from '@react-three/drei'
 import {Model} from './Model'
 
 export function MainCanvas() {
   return (
     <Canvas>
+      <ambientLight intensity={0.5} />
+      <CameraControls makeDefault />
       <Model />
     </Canvas>
   )
