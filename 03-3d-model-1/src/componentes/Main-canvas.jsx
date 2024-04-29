0e351541bd2aeaf21162d5885093dc3006f173cb
Marco Rossel <andresmarcorossel@gmail.com>
2024-04-29 15:57:37 -0400

    ts three react 3 | 06 clone cube

--- 03-3d-model-1/src/componentes/Main-canvas.jsx
+++ 03-3d-model-1/src/componentes/Main-canvas.jsx
@@ -1,12 +1,39 @@
 import {Canvas} from '@react-three/fiber'
-import { CameraControls } from '@react-three/drei'
+import { CameraControls, Edges } from '@react-three/drei'
 import {Model} from './Model'
+import {Side} from './Side'
 
 export function MainCanvas() {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
+    <Canvas shadows camera={{ position: [-3, 2.5, 3] }}> 
+      {/* camera=nos ubica en la posicion inicial al cargar la pagina */}
+
+      {/* ambientLight = luz ambiental, si no se aplica se ve todo negro, deve ir por defecto para ver colores y materiales */}
+      <ambientLight intensity={2.5} />
+      {/* CameraControls = nos permite orbitar el objeto con el mouse */}
       <CameraControls makeDefault />
+      <mesh castShadow receiveShadow>
+        <boxGeometry args={[2, 2, 2]} />
+        <Edges />
+        <Side rotation={[0, 0, 0]} bg="orange" index={0}>
+          <torusGeometry args={[0.65, 0.3, 64]} />
+        </Side>
+        <Side rotation={[0, Math.PI, 0]} bg="lightblue" index={1}>
+          <torusKnotGeometry args={[0.55, 0.2, 128, 32]} />
+        </Side>
+        <Side rotation={[0, Math.PI / 2, Math.PI / 2]} bg="lightgreen" index={2}>
+          <boxGeometry args={[1.15, 1.15, 1.15]} />
+        </Side>
+        <Side rotation={[0, Math.PI / 2, -Math.PI / 2]} bg="aquamarine" index={3}>
+          <octahedronGeometry />
+        </Side>
+        <Side rotation={[0, -Math.PI / 2, 0]} bg="indianred" index={4}>
+          <icosahedronGeometry />
+        </Side>
+        <Side rotation={[0, Math.PI / 2, 0]} bg="hotpink" index={5}>
+          <dodecahedronGeometry />
+        </Side>
+      </mesh>
       <Model />
     </Canvas>
   )
---
+++ 03-3d-model-1/src/componentes/Side.jsx
@@ -0,0 +1,29 @@
+import { Environment, MeshPortalMaterial, useGLTF } from "@react-three/drei"
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+
+export function Side({ rotation = [0, 0, 0], bg = '#f0f0f0', children, index }) {
+  const mesh = useRef()
+  //const { worldUnits } = useControls({ worldUnits: false })
+  const { nodes } = useGLTF('public/model/aobox-transformed.glb')
+  useFrame((state, delta) => {
+    mesh.current.rotation.x = mesh.current.rotation.y += delta
+  })
+  return (
+    <MeshPortalMaterial /* worldUnits={worldUnits} */ attach={`material-${index}`}>
+      {/** Everything in here is inside the portal and isolated from the canvas */}
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+      {/** A box with baked AO */}
+      <mesh castShadow receiveShadow rotation={rotation} geometry={nodes.Cube.geometry}>
+        <meshStandardMaterial aoMapIntensity={1} aoMap={nodes.Cube.material.aoMap} color={bg} />
+        <spotLight castShadow color={bg} intensity={2} position={[10, 10, 10]} angle={0.15} penumbra={1} shadow-normalBias={0.05} shadow-bias={0.0001} />
+      </mesh>
+      {/** The shape */}
+      <mesh castShadow receiveShadow ref={mesh}>
+        {children}
+        <meshLambertMaterial color={bg} />
+      </mesh>
+    </MeshPortalMaterial>
+  )
+}
