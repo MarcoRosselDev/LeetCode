2e08aed825535d569cce8d44f6c0aad0faf19366
Marco Rossel <andresmarcorossel@gmail.com>
2024-04-30 00:24:12 -0400

    ts three react 3 | 08-2

--- 01-ts-react-three/src/contexto/TemaGlobal.tsx
+++ 01-ts-react-three/src/contexto/TemaGlobal.tsx
@@ -2,10 +2,9 @@ import {useState } from "react";
 import {context} from './Context'
 
 export type Tema = 'dark' | 'ligth' | 'purple'
+
 export function TemaGlobal({children}: {children: React.ReactNode}) {
   const [dark, setDark] = useState('dark')
-
-
   function handleDark(value: Tema):void {
     setDark(value)
   }
