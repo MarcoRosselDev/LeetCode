e1a31efebe2df409c0ea03eabc1e541edc65b5d1
Marco Rossel <andresmarcorossel@gmail.com>
2024-05-01 18:05:15 -0400

    ts three react 4 | 03

--- 04-cubo-magico-2/src/App.tsx
+++ 04-cubo-magico-2/src/App.tsx
@@ -1,9 +1,12 @@
 import { Canvas } from '@react-three/fiber'
+import { CameraControls} from '@react-three/drei'
 
 const App = () => (
   <Canvas>
-    <pointLight position={[1, 1, 1]} intensity={3}/>
-    <ambientLight intensity={0.5}></ambientLight>
+    <CameraControls makeDefault />
+    <pointLight position={[1, 1.5, 2]} intensity={3}/>
+    {/* <ambientLight intensity={0.5}></ambientLight> */}
+    {/* <orthographic ></orthographic> */}
     <mesh>
       <sphereGeometry />
       <meshStandardMaterial color="red" />
