b4ba400ad6fa6e1db4645e16c6da2bf790380298
Marco Rossel <andresmarcorossel@gmail.com>
2024-05-09 00:42:48 -0400

    three 5 | vanilla manual 01

--- 05-three-vanilla/main_2.js
+++ 05-three-vanilla/main_2.js
@@ -1,9 +1,10 @@
 import * as THREE from 'three'
+import './style.css'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth/2, window.innerHeight/2, false );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
