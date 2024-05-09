928cb2ef21478a862250ba17d95a71fcd7396e92
Marco Rossel <andresmarcorossel@gmail.com>
2024-05-09 02:43:36 -0400

    three 5 | vanilla manual 03

--- 05-three-vanilla/lineas.js
+++ 05-three-vanilla/lineas.js
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 import './style.css'
 
 const render_linea = new THREE.WebGLRenderer()
-render_linea.setSize(window.innerWidth/3, window.innerHeight/3 );
+render_linea.setSize(window.innerWidth/1, window.innerHeight/1 );
 document.body.appendChild( render_linea.domElement );
 const camara = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 500)
 camara.position.set(0,0,100)
@@ -10,22 +10,36 @@ camara.lookAt(0,0,0)
 
 const scene = new THREE.Scene()
 
-const material = new THREE.LineBasicMaterial( { color: 0xffff } );
+const material = new THREE.LineBasicMaterial( { color: 0xffff } );// supongo que se puede cambiar a una linea de trazo para un aspecto de dibujo
 const points = [];
 points.push( new THREE.Vector3( - 10, 0, 0 ) );
 points.push( new THREE.Vector3( 0, 10, 0 ) );
 points.push( new THREE.Vector3( 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 0, 10 ) );
+points.push( new THREE.Vector3( 0, -10, 0 ) );
+points.push( new THREE.Vector3( 20, 1, 0 ) );
 
 const geometry = new THREE.BufferGeometry().setFromPoints( points );
 
 const line = new THREE.Line( geometry, material );
 
+//const cubo = new THREE.Mesh( geometry, material );
+//new THREE.MeshBasicMaterial( { color: 0x00ff }
+const cubo = new THREE.Mesh( new THREE.BoxGeometry( 10,10, 10),  new THREE.MeshNormalMaterial());
+
+scene.add(cubo)
 scene.add(line)
+
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+
+scene.add( light );
 
 function render_scene() {
   requestAnimationFrame(render_scene)
   line.rotation.y += 0.01;
   line.rotation.x += 0.01;
+  cubo.rotation.y += 0.01;
+  cubo.rotation.x += 0.01;
   render_linea.render(scene, camara)
 }
 
