ba07f56b217b403529ebae63a28da9ad75bf9bbf
Marco Rossel <andresmarcorossel@gmail.com>
2024-05-11 22:25:50 -0400

    three 6 | vanilla manual 02

--- 05-three-vanilla/three_fundamentals.js
+++ 05-three-vanilla/three_fundamentals.js
@@ -6,20 +6,19 @@ import * as THREE from 'three'
 
 function main() {
   //const c = document.querySelector('#c')
-  const c = document.querySelector('.c')
-  console.log(c);
-  const rendereer = new THREE.WebGLRenderer({antialias: true, c});
-  const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 50)
+  const canvas = document.querySelector('.c')// no puedo llamar de otra forma a esta variable
+
+  const renderer = new THREE.WebGLRenderer({antialias: true, canvas});// si a canvas lo llamo c no funka
+  const camara = new THREE.PerspectiveCamera(75, 2, 0.1, 7)
+  camara.position.z = 2
   //const rendereer = new THREE.WebGLRenderer({antialias: true, c})
+  const scene = new THREE.Scene()
   
-  camera.position.z = 2
   const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
   const geometria = new THREE.BoxGeometry(1, 1, 1)//
   const cubo = new THREE.Mesh(geometria, material)
-  console.log(cubo);
-  const scene = new THREE.Scene()
   scene.add(cubo)
-  rendereer.render(scene, camera)
+  renderer.render(scene, camara)
   console.log('main');
 }
 
