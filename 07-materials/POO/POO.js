70c4308e5ea2a56e3e9f1eb997f56068538635e0
Marco Rossel <andresmarcorossel@gmail.com>
2024-05-21 13:10:25 -0400

    three 7 | vanilla manual | othographic camara

--- 07-materials/POO/POO.js
+++ 07-materials/POO/POO.js
@@ -8,17 +8,27 @@ function Persona(nombre, edad) {
   }
 }
 
+const persona_const = {
+  nombre: 'marco',
+  edad: 31,
+  saludar: function () {
+    return `Hola, buenos dias. Yo soy ${this.nombre}`
+  }
+}
+
+console.log(persona_const.saludar());
+
 class Persona_class {
   constructor(name) {
     this.name = name
   }
-  mover_brazo = function () {
+  mover_brazo () {
     return `Mover brazo de ${this.name}`
   }
 }
 
 const persona_2 = new Persona_class('marco')
-console.log(persona_2.mover_brazo());
+console.log(persona_2.mover_brazo(``));
 
 const persona_1 = new Persona('marco', 31)
 console.log(persona_1.upperName()); // MARCO
---
+++ 07-materials/camara/camara_perspectiva.js
@@ -0,0 +1,42 @@
+import resizeRendererToDisplaySize from '../resize'
+import './style.css'
+import * as THREE from 'three'
+
+const canvas = document.getElementById('c')
+const renderer = new THREE.WebGLRenderer({canvas, antialias: true, alpha: true, premultipliedAlpha: false})
+
+const scene = new THREE.Scene()
+const camara = new THREE.PerspectiveCamera(50, 1, 0.1, 30)
+camara.position.z = 4
+
+//rectangulos
+const geometria_rectangulo = new THREE.BoxGeometry(2,1,1)
+const material_rect_1 = new THREE.MeshPhongMaterial({color: 'lightGreen', shininess: 900})
+const mesh_rect_1 = new THREE.Mesh(geometria_rectangulo, material_rect_1)
+mesh_rect_1.position.set(0,0,-3)
+
+//luz ambiental y direccional
+const luz_ambiente = new THREE.AmbientLight('white', 0.1)
+const luz_direccional = new THREE.DirectionalLight('white', 1)
+luz_direccional.position.y = 10
+luz_direccional.position.x = 5
+
+scene.add(mesh_rect_1, luz_ambiente, luz_direccional)
+
+//console.log(time);
+function animacion(time) {// al parecer time es un derivado de requestAnimationFrame
+  // de otra froma log time nos arojara un error
+  const tiempo = time*0.001
+  mesh_rect_1.rotation.y = tiempo
+  mesh_rect_1.rotation.z = tiempo
+
+  // actualizar aspecto del canvas
+  camara.aspect = canvas.clientWidth / canvas.clientHeight;
+  camara.updateProjectionMatrix();
+  resizeRendererToDisplaySize(renderer)// resize
+
+  renderer.render(scene, camara)
+  
+  requestAnimationFrame(animacion)
+}
+animacion()
