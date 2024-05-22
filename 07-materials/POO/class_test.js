a07dd8b6a77dc9e64ed7005081f5aa88cfcbe1c7
Marco Rossel <andresmarcorossel@gmail.com>
2024-05-22 06:18:42 -0400

    three 7 | vanilla manual | poo reminder

---
+++ 07-materials/POO/class_test.js
@@ -0,0 +1,17 @@
+class Persona {
+  constructor(edad, nombre, altura){
+    this.edad = edad
+    this.nombre = nombre
+    this.altura = altura
+  }
+  get_nombre(){
+    return `Hola mi nombre es ${this.nombre}`
+  }
+  get_altura(str){
+    return `Hola, yo mido ${this.altura} y me gusta el ${str}`
+  }
+}
+
+const marco = new Persona(31, 'marco', 1.73)
+
+console.log(marco.get_altura('arroz con huevo'));
