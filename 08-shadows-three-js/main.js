48a011e6cf24a13dfeb569612f602920f6c11491
Marco Rossel <andresmarcorossel@gmail.com>
2024-06-04 03:48:17 -0400

    three 8 | shadows | 01

--- 08-shadows-three-js/main.js
+++ 08-shadows-three-js/main.js
@@ -9,7 +9,8 @@ const renderer = new THREE.WebGLRenderer({
 })
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 20)
-camera.position.z = 4 // retrocedemos la posicion de la camera 4 unidades para ver mejor
+camera.position.z = 10 // retrocedemos la posicion de la camera 4 unidades para ver mejor
+camera.position.y = 2 // retrocedemos la posicion de la camera 4 unidades para ver mejor
 
 const sphera_geometry = new THREE.SphereGeometry(1, 10, 10)
 const sphera_material = new THREE.MeshPhongMaterial({
@@ -23,8 +24,14 @@ const luz_direccional = new THREE.DirectionalLight('white', 2)
 luz_direccional.position.x = 3
 luz_direccional.position.y = 3
 luz_direccional.position.z = 2
+//pizo
+const pizo_geo = new THREE.BoxGeometry(10,10,0.01)
+const pizo_material = new THREE.MeshPhongMaterial({color: 'coral', shininess: 700})
+const pizo_mesh = new THREE.Mesh(pizo_geo, pizo_material)
+pizo_mesh.rotateX(Math.PI/2)
+pizo_mesh.position.y = -1
 
-scene.add(sphera_mesh, luz_ambiental, luz_direccional)
+scene.add(sphera_mesh, luz_ambiental, luz_direccional, pizo_mesh)
 
 function animation() {
   sphera_mesh.rotation.x += 0.01
