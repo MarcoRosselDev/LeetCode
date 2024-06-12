84b8de32229b09fe2710d8806a15d7a1bfd52de4
Marco Rossel <andresmarcorossel@gmail.com>
2024-06-12 17:38:58 -0400

    nav var | 12

--- 09-nav-var/header.js
+++ 09-nav-var/header.js
@@ -1,15 +1,16 @@
 const div_sub = document.querySelector('.div_sub')
 
 const item_a = document.querySelector('.item_a')
-item_a.addEventListener('click', function (event) {
+item_a.addEventListener('mouseover', function (event) {
   event.preventDefault()
 //  console.log(this.parentElement.parentElement);
   div_sub.innerHTML = `
-  <p>sub item a</p>
-  <p>sub item a</p>
+  <p><a href="http://marcorossel.com" target="_blank">marco rossel portfollio</a></p>
+  <p><a href="http://youtube.com" target="_blank">youtube</a></p>
   <p>sub item a</p>
   <p>sub item a</p>
   <p>sub item a</p>`
+  div_sub.classList.add('padd')
 })
 
 const header = document.querySelector('header')
@@ -17,4 +18,17 @@ const header = document.querySelector('header')
 header.addEventListener('mouseleave', function (e) {
   e.preventDefault()
   div_sub.innerHTML = ''
+  div_sub.className = 'div_sub'
+})
+
+const item_b = document.querySelector('.item_b')
+item_b.addEventListener('mouseover', (event) =>{
+  event.preventDefault()
+
+  div_sub.innerHTML = `
+  <p><a href="http://marcorossel.com" target="_blank">marco rossel portfollio b</a></p>
+  <p><a href="http://youtube.com" target="_blank">youtube b</a></p>
+  <p>sub item b</p>
+  <p>sub item b</p>`
+  div_sub.classList.add('padd')
 })
