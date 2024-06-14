db19057d133fd7030d995f69f5905ab723f55b22
Marco Rossel <andresmarcorossel@gmail.com>
2024-06-14 13:23:33 -0400

    nav var | 17

--- 09-nav-var/header.js
+++ 09-nav-var/header.js
@@ -34,12 +34,20 @@ const pricing = document.querySelector('.pricing')
 pricing.addEventListener('mouseover', (e) => {
   e.preventDefault()
   div_sub.innerHTML = `
-  <p><a href="http://marcorossel.com" target="_blank">marco rossel portfollio c</a></p>
-  <p><a href="http://youtube.com" target="_blank">youtube c</a></p>
-  <p>sub item c</p>
-  <p>sub item c</p>
+  <div class="item_1">
+    <h4>Free vs. Commercial</h4>
+    <p>Needle Engine and Integrations can be evaluated for free. Please see our commercial plans below for more information</p>
+    <button class="purple-button">See Commercial Plans</button>
+  </div>
+  <div class="item_2">
+    <h4>More Info</h4>
+    <p>The Small Print</p>
+    <p><a href="">License and Terms</a></p>
+    <p><a href="">Pricing</a></p>
+  </div>
   `
   div_sub.classList.add('padd')
+  div_sub.classList.add('pricing')
 })
 
 const company = document.querySelector('.company')
@@ -54,11 +62,11 @@ company.addEventListener('mouseover', (e) => {
         </div>
         <div class="item_2">
           <h4><a>Say hi!</a></h4>
-          <p><a>Newletter signup</a></p>
-          <p><a>Twitter</a></p>
-          <p><a>Discord community</a></p>
-          <p><a>GitHub</a></p>
-          <p><a>Send us an e-mail</a></p>
+          <p><a href="">Newletter signup</a></p>
+          <p><a href="">Twitter</a></p>
+          <p><a href="">Discord community</a></p>
+          <p><a href="">GitHub</a></p>
+          <p><a href="">Send us an e-mail</a></p>
         </div>
         <div class="item_3">
           <h4>Other Products</h4>
