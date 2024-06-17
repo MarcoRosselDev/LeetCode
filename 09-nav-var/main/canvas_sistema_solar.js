45b1e6e16e85d12250c5e266d4ba2c139c7d24b0
Marco Rossel <andresmarcorossel@gmail.com>
2024-06-16 22:21:00 -0400

    nav var | 19

--- 09-nav-var/main/canvas_sistema_solar.js
+++ 09-nav-var/main/canvas_sistema_solar.js
@@ -6,7 +6,7 @@ const renderer = new THREE.WebGLRenderer({canvas, antialias: true, alpha: true})
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 30)
-camera.position.z = 8
+camera.position.z = 3
 const luz_ambiental = new THREE.AmbientLight(0xffffff, 0.5)
 const luz_direccional = new THREE.DirectionalLight(0xffffff, 1)
 luz_ambiental.position.set(2,2,2)
@@ -15,14 +15,32 @@ const sphera_geometry = new THREE.SphereGeometry(1,10,10)
 const sol_material = new THREE.MeshPhongMaterial({color: 'yellow',})
 const sol_mesh = new THREE.Mesh(sphera_geometry, sol_material)
 
+const solar_sistem = new THREE.Object3D()
+const tierra_sistem = new THREE.Object3D()
+solar_sistem.add(sol_mesh)
+console.log(solar_sistem);
+
+const tierra_material = new THREE.MeshPhongMaterial({color: 'blue'})
+const mesh_tierra = new THREE.Mesh(sphera_geometry, tierra_material)
+mesh_tierra.position.x = 2
+mesh_tierra.scale.set(0.3,0.3,0.3)
+solar_sistem.add(mesh_tierra)
+
+const moon_material = new THREE.MeshPhongMaterial({color: 'green'})
+const mesh_moon = new THREE.Mesh(sphera_geometry, moon_material)
+tierra_sistem.add(mesh_moon)
+
+
 scene.add(
-  sol_mesh,
+  solar_sistem,
   luz_ambiental,
   luz_direccional,
 )
 
 function animation() {
-  sol_mesh.rotation.y += 0.005
+  solar_sistem.rotation.y += 0.005
+  mesh_tierra.rotation.y += 0.005
+  mesh_tierra.rotation.x += 0.005
 
   renderer.render(scene, camera)
   resizeRendererToDisplaySize(renderer)
