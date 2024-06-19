add7b70e6a8c24c1b582593e548995ce7eb8f0da
Marco Rossel <andresmarcorossel@gmail.com>
2024-06-19 03:39:40 -0400

    nav var | 22

---
+++ 09-nav-var/main/cargar_obj.js
@@ -0,0 +1,32 @@
+import * as THREE from 'three'
+import resizeRendererToDisplaySize from './resize.js'
+import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
+console.log(OBJLoader);
+
+const canvas = document.getElementById('c')
+const renderer = new THREE.WebGLRenderer({canvas, alpha: true, antialias: true})
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 30)
+const scene = new THREE.Scene()
+const ambiental_ligth = new THREE.AmbientLight(0xffffff, 1)
+
+camera.position.z = 8
+
+const structure_box = new THREE.BoxGeometry(1,1,1)
+const material_box = new THREE.MeshPhongMaterial({color:'lightblue'})
+const mesh_box = new THREE.Mesh(structure_box, material_box)
+
+scene.add(
+  mesh_box,
+  ambiental_ligth,
+)
+
+function animation() {
+  mesh_box.rotation.x += 0.005
+  mesh_box.rotation.y += 0.005
+
+  resizeRendererToDisplaySize(renderer)
+  requestAnimationFrame(animation)
+  renderer.render(scene, camera)
+}
+
+animation()
