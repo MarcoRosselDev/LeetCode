cdbb3513277a53f9c7b21d43ba1cd8b0ee288bd9
Marco Rossel <andresmarcorossel@gmail.com>
2025-02-09 06:18:27 -0300

    mdn | adivina el numero 00

---
+++ mdn-web-docs/dinamic-scriptin-with-js/juego-adivina-el-numero/script.js
@@ -0,0 +1,7 @@
+const enviar_respuesat = document.querySelector(".enviar_respuesta");
+const mi_numero = document.querySelector(".mi_numero");
+
+enviar_respuesat.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("hola mundo");
+});
